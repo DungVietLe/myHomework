b34e541d840040fec7f1e70b8a1d3f1292bf4f05
DungVietLe <74787642+DungVietLe@users.noreply.github.com>
2021-05-12 22:06:47 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+/**Cau 1 */
+
+// let getName = prompt('Nhap ten cua ban : ');
+// let getAge = Number(prompt('Nhap tuoi cua ban: '));
+
+// console.log(alert(`Hi ${getName} , tuoi cua ban la ${getAge} , chuc ban 1 ngay tot lanh <3`));
+
+
+
+/**cau 2 */
+
+
+// let getFistName = prompt('Please press ur First Name :');
+// let getLastName = prompt('Please pess ur Last Name :');
+
+// console.log(alert(`Hi ${getFistName} ${getLastName}`));
+
+
+/**Cau 3 */
+
+// let getA = Number(prompt('canh A:'));
+
+
+// let getS = getA * getA;
+
+// console.log(alert(`dien tich hinh vuong = ${getS}`));
+
+/**Cau 4 */
+
+// let banKinh = Number(prompt('Nhap ban kinh hinh tron :'));
+
+
+// let dienTichHinhTron = banKinh * banKinh * Math.PI;
+
+// console.log(alert(`dien tich hinh tron = ${dienTichHinhTron}`));
+
+/**Cau 5 */
+
+// let doC = Number(prompt('Nhap gia tri do C :'));
+
+// let convert = doC * 9/5 + 32 ; 
+
+// console.log(alert(`do F = ${convert}`));
